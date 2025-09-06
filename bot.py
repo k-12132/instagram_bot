c1cf190bd60b8c9a43954d66db199f60880d7a71
k-12132 <103574686+k-12132@users.noreply.github.com>
2025-09-06 22:07:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-# متغير التوكن
+# التوكن
 TOKEN = os.environ.get("BOT_TOKEN")
 
 # إعدادات Render
@@ -18,14 +18,13 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"لقد أرسلت: {update.message.text}")
 
 if __name__ == "__main__":
-    # إنشاء التطبيق
     app = Application.builder().token(TOKEN).build()
 
-    # إضافة الـ Handlers
+    # إضافة Handlers
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # تشغيل Webhook على Render
+    # تشغيل Webhook
     app.run_webhook(
         listen=HOST,
         port=PORT,
