c1aba743be6efbcad939d795c730a972fedc1906
k-12132 <103574686+k-12132@users.noreply.github.com>
2025-09-06 21:50:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")
-PORT = int(os.environ.get("PORT", 8443))  # رقم البورت
+TOKEN = os.environ["BOT_TOKEN"]
+PORT = int(os.environ.get("PORT", 8443))
 HOST = "0.0.0.0"
-WEBHOOK_URL = f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}"
+WEBHOOK_URL = f"https://{os.environ['RENDER_EXTERNAL_HOSTNAME']}/{TOKEN}"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("بوتك يعمل بنجاح ✅")
+    await update.message.reply_text("بوتك يعمل ✅")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"لقد أرسلت: {update.message.text}")
@@ -23,7 +23,6 @@ if __name__ == "__main__":
     app.run_webhook(
         listen=HOST,
         port=PORT,
-        webhook_path=f"/{TOKEN}",
         url_path=TOKEN,
         webhook_url=WEBHOOK_URL
     )
